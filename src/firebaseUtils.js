6f8a5b3c3853cb6063ae2ce305272252b739866d
Bibhu20031 <bibhusmaran@gmail.com>
2024-02-12 15:03:46 +0530

    auth changes

--- src/firebaseUtils.js
+++ src/firebaseUtils.js
@@ -12,24 +12,6 @@ const firebaseConfig = {
 
 
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDdk78sbOWBbGTclZhAPou3xkUAS5NE4g8",
-//   authDomain: "react-ecommerce-d13c1.firebaseapp.com",
-//   projectId: "react-ecommerce-d13c1",
-//   storageBucket: "react-ecommerce-d13c1.appspot.com",
-//   messagingSenderId: "1070320056926",
-//   appId: "1:1070320056926:web:2ff2ae49071853790212cf"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
 
 
 
